28a941219c35a766df8be4919a9fbe0b84b6a229
mvillalobos712 <mvillalobos712@gmail.com>
2017-05-25 14:19:11 -0400

    Create ej2.py

---
+++ ej2.py
@@ -0,0 +1,16 @@
+descripcion: ejecutar programa que le pregunte al usuario  si el numero es par o impar
+entrada: preguntar al usuario si el numero es par n = int(input("ingrese un numero /n:"))
+salida: numero par o impar con str
+autor:
+fecha:25-05-2017
+version:1.0 
+plataforma: python v2.7
+n = int(input("ingrese un numero /N:"))
+if (n%2 :== 0):
+ print(str(n)+"es par")
+ else:
+  print(str(n)+"es impar")
+  
+ 
+ 
+
